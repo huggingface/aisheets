4e3aff2557c58779234b87d0e61368bb53990fda
Daniel Vila Suero <daniel.vila@huggingface.co>
2025-02-10 14:55:43 +0100

    Update src/routes/auth/callback/index.ts
    Co-authored-by: Paco Aranda <francisco.aranda@huggingface.co>

--- src/routes/auth/callback/index.ts
+++ src/routes/auth/callback/index.ts
@@ -40,7 +40,6 @@ export const onGet = async ({ cookie, redirect, query, url }: RequestEvent) => {
         username: auth.userInfo.preferred_username,
         picture: auth.userInfo.picture,
       },
-      inferenceProvider: process.env.INFERENCE_PROVIDER || 'hf-inference',
     };
 
     saveSession(cookie, session);

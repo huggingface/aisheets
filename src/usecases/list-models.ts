93a4d5c41c962047f1b24f842f328f5c14b7a2a5
Daniel Vila Suero <daniel.vila@huggingface.co>
2025-02-10 11:24:12 +0100

    Update src/usecases/list-models.ts
    Co-authored-by: Paco Aranda <francisco.aranda@huggingface.co>

--- src/usecases/list-models.ts
+++ src/usecases/list-models.ts
@@ -11,7 +11,7 @@ export const listModels = server$(async function (
   this: RequestEventBase<QwikCityPlatform>,
 ): Promise<Model[]> {
   const session = useServerSession(this);
-  const MODEL_URL = `https://huggingface.co/api/models?inference_provider=${session.inferenceProvider}&pipeline_tag=text-generation`;
+  const MODEL_URL = `https://huggingface.co/api/models?inference_provider=${session.inferenceProvider}&pipeline_tag=text-generation&sort=trendingScore&direction=-1`;
   const response = await fetch(MODEL_URL);
 
   if (!response.ok) {

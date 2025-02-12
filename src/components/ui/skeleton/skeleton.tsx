457beb63e8915b4283c7ec55d1b7d86565e0293d
Damián Pumar <damianpumar@gmail.com>
2025-02-12 09:53:05 +0100

    fix: :art: Adjust padding and margin for table cells and skeleton component

--- src/components/ui/skeleton/skeleton.tsx
+++ src/components/ui/skeleton/skeleton.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export const Skeleton = component$(() => {
   return (
-    <div class="flex items-start py-10 h-full space-x-1">
+    <div class="flex items-start h-full py-4 space-x-1">
       <div class="w-[4px] h-[4px] bg-ring rounded-full animate-[bounce_1.4s_infinite]" />
       <div class="w-[4px] h-[4px] bg-ring rounded-full animate-[bounce_1.4s_infinite_0.2s]" />
       <div class="w-[4px] h-[4px] bg-ring rounded-full animate-[bounce_1.4s_infinite_0.4s]" />

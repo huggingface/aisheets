bad7434e69a8307099f5c02ccad2e88974afc7e0
Francisco Aranda <frascuchon@gmail.com>
2025-02-13 12:08:55 +0100

    import-from-hub-usecase

---
+++ src/usecases/import-from-hub.usecase.ts
@@ -0,0 +1,107 @@
+import { type RequestEventBase, server$ } from '@builder.io/qwik-city';
+import { type Dataset, useServerSession } from '~/state';
+
+import consola from 'consola';
+import { createCell, createColumn, createDataset } from '~/services';
+import {
+  describeDatasetSplit,
+  getDatasetInfo,
+  loadDatasetRows,
+} from '~/services/repository/hub';
+
+export interface ImportFromHubParams {
+  repoId: string;
+  subset?: string;
+  split?: string;
+}
+
+export const useImportFromHub = () =>
+  server$(async function (
+    this: RequestEventBase<QwikCityPlatform>,
+    importParams: ImportFromHubParams,
+  ): Promise<Dataset> {
+    const { repoId, subset, split } = importParams;
+    const session = useServerSession(this);
+
+    consola.info('Getting dataset info for repoId:', repoId);
+    const datasetInfo = await getDatasetInfo({
+      repoId,
+      accessToken: session.token,
+    });
+
+    const selectedSubset =
+      (subset
+        ? datasetInfo.subsets.find((s) => s.name === subset)
+        : undefined) || datasetInfo.subsets[0];
+    const selectedSplit =
+      (split
+        ? selectedSubset.splits.find((s) => s.name === split)
+        : undefined) || selectedSubset.splits[0];
+
+    consola.info(
+      'Describing split columns',
+      repoId,
+      selectedSubset.name,
+      selectedSplit.name,
+    );
+    const splitColumns = await describeDatasetSplit({
+      repoId,
+      subset: selectedSubset.name,
+      split: selectedSplit.name,
+      accessToken: session.token,
+    });
+
+    const supportedColumns = splitColumns.filter(
+      (col) => col.type === 'VARCHAR',
+    );
+
+    if (supportedColumns.length === 0) {
+      throw new Error('No supported columns found');
+    }
+
+    consola.info('Creating Dataset...');
+    const createdDataset = await createDataset({
+      name: repoId,
+      // TODO: pass the user instead of the username and let the repository handle the createdBy
+      createdBy: session.user.username,
+    });
+
+    consola.info('Creating columns...');
+    for (const column of supportedColumns) {
+      const createdColumn = await createColumn({
+        dataset: createdDataset,
+        name: column.name,
+        type: 'text',
+        kind: 'dynamic',
+      });
+      createdDataset.columns.push(createdColumn);
+    }
+
+    consola.info('Loading dataset rows');
+    const { rows } = await loadDatasetRows({
+      repoId,
+      accessToken: session.token,
+      parquetFiles: [selectedSplit.files[0]],
+      limit: 100,
+      // TODO: Add column names as a parameter
+    });
+
+    consola.info('Creating cells...');
+    for (const row of rows) {
+      for (const column of createdDataset.columns) {
+        const value = row[column.name];
+
+        const createdCell = await createCell({
+          cell: {
+            idx: row.idx,
+            value: value,
+          },
+          column,
+        });
+
+        column.cells.push(createdCell);
+      }
+    }
+    consola.info('Dataset created:', createdDataset);
+    return createdDataset;
+  });

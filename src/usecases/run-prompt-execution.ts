c3a6d74aa8dbf20495aded165625ad19290c04f2
Francisco Aranda <frascuchon@gmail.com>
2025-02-20 09:41:55 +0100

    remove attribute

--- src/usecases/run-prompt-execution.ts
+++ src/usecases/run-prompt-execution.ts
@@ -166,7 +166,6 @@ export const runPromptExecution = async ({
         accessToken,
       ),
       {
-        use_cache: false,
         signal: AbortSignal.timeout(timeout ?? DEFAULT_TIMEOUT),
       },
     );

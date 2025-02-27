935407ae709c79f479439763ea96d20d6343e366
Damián Pumar <damianpumar@gmail.com>
2025-02-27 23:35:55 +0100

    Implement virtual scrolling for table body and optimize cell rendering

--- src/features/table/table-body.tsx
+++ src/features/table/table-body.tsx
@@ -1,8 +1,12 @@
 import {
+  $,
   Fragment,
   component$,
   useComputed$,
+  useOnWindow,
   useSignal,
+  useTask$,
+  useVisibleTask$,
 } from '@builder.io/qwik';
 import { useExecution } from '~/features/add-column';
 import { TableCell } from '~/features/table/table-cell';
@@ -13,76 +17,106 @@ export const TableBody = component$(() => {
   const { columnId } = useExecution();
   const expandedRows = useSignal<Set<number>>(new Set());
 
-  const getCell = (column: Column, rowIndex: number): Cell => {
-    const cell = column.cells[rowIndex];
+  const tableBody = useSignal<HTMLElement>();
+  const rowHeight = 21;
+  const visibleRows = 9;
+  const buffer = 9;
 
-    if (!cell) {
-      // Temporal cell for skeleton
-      return {
-        id: `${column.id}-${rowIndex}`,
-        value: '',
-        error: '',
-        validated: false,
-        column: {
-          id: column.id,
-        },
-        updatedAt: new Date(),
-        generated: false,
-        idx: rowIndex,
-      };
-    }
-
-    return cell;
-  };
+  const scrollTop = useSignal(0);
+  const startIndex = useSignal(0);
+  const endIndex = useSignal(0);
 
   const rowCount = useComputed$(() => {
-    if (!firstColum.value.process)
-      throw new Error('No process, review this for static columns');
-
-    return firstColum.value.process.limit;
+    return firstColum.value.process?.limit || firstColum.value.cells.length;
   });
 
+  useVisibleTask$(({ track }) => {
+    track(scrollTop);
+    startIndex.value = Math.max(
+      Math.floor(scrollTop.value / rowHeight) - buffer,
+      0,
+    );
+
+    endIndex.value = Math.min(
+      startIndex.value + visibleRows + buffer,
+      rowCount.value,
+    );
+  });
+
+  const data = useSignal<Cell[][]>([]);
+  useTask$(({ track }) => {
+    track(columns);
+
+    const getCell = (column: Column, rowIndex: number): Cell => {
+      const cell = column.cells[rowIndex];
+
+      if (!cell) {
+        // Temporal cell for skeleton
+        return {
+          id: `${column.id}-${rowIndex}`,
+          value: '',
+          error: '',
+          validated: false,
+          column: {
+            id: column.id,
+          },
+          updatedAt: new Date(),
+          generated: false,
+          idx: rowIndex,
+        };
+      }
+
+      return cell;
+    };
+
+    data.value = Array.from({ length: rowCount.value }, (_, rowIndex) =>
+      Array.from({ length: columns.value.length }, (_, colIndex) =>
+        getCell(columns.value[colIndex], rowIndex),
+      ),
+    );
+  });
+
+  useOnWindow(
+    'scroll',
+    $((event) => {
+      scrollTop.value =
+        (event.target as HTMLElement).scrollTop - tableBody.value!.offsetTop;
+    }),
+  );
+
   return (
-    <tbody>
-      {Array.from({ length: rowCount.value }).map((_, rowIndex) => (
-        <tr key={rowIndex} class="hover:bg-gray-50/50 transition-colors">
-          {columns.value.map((column) => {
-            const cell = getCell(column, rowIndex);
-
-            return (
-              <Fragment key={`${column.id}-${rowIndex}-${cell.id}`}>
-                {column.id === TEMPORAL_ID ? (
-                  <td
-                    key={`temporal-${rowIndex}`}
-                    class="min-w-80 w-80 max-w-80 px-2 min-h-[100px] h-[100px] border-[0.5px]"
-                  />
-                ) : (
-                  <TableCell
-                    cell={cell}
-                    isExpanded={expandedRows.value.has(rowIndex)}
-                    onToggleExpand$={() => {
-                      const newSet = new Set(expandedRows.value);
-                      if (newSet.has(rowIndex)) {
-                        newSet.delete(rowIndex);
-                      } else {
-                        newSet.add(rowIndex);
-                      }
-                      expandedRows.value = newSet;
-                    }}
-                  />
-                )}
-
-                {columnId.value === column.id && (
-                  <td class="min-w-[600px] w-[600px] bg-white" />
-                )}
-              </Fragment>
-            );
-          })}
-
-          {/* td for (add + ) column */}
-          <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-r-0" />
-        </tr>
-      ))}
+    <tbody ref={tableBody}>
+      {data.value
+        .slice(startIndex.value, endIndex.value)
+        .map((cells, rowIndex) => (
+          <tr key={rowIndex} class="hover:bg-gray-50/50 transition-colors">
+            {cells.map((cell) => {
+              return (
+                <Fragment key={`${rowIndex}-${cell.id}`}>
+                  {cell.column?.id === TEMPORAL_ID ? (
+                    <td class="min-w-80 w-80 max-w-80 px-2 min-h-[100px] h-[100px] border-[0.5px]" />
+                  ) : (
+                    <TableCell
+                      cell={cell}
+                      isExpanded={expandedRows.value.has(rowIndex)}
+                      onToggleExpand$={() => {
+                        const newSet = new Set(expandedRows.value);
+                        newSet.has(rowIndex)
+                          ? newSet.delete(rowIndex)
+                          : newSet.add(rowIndex);
+                        expandedRows.value = newSet;
+                      }}
+                    />
+                  )}
+                  {columnId.value === cell.column?.id && (
+                    <td class="min-w-[600px] w-[600px] bg-white" />
+                  )}
+                </Fragment>
+              );
+            })}
+            <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-r-0" />
+          </tr>
+        ))}
     </tbody>
   );
 });

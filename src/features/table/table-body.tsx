e3693142571d7254efade39f854fd1bca1466e39
Damián Pumar <damianpumar@gmail.com>
2025-02-28 13:03:57 +0100

    Add virtual scrolling implementation for table component

--- src/features/table/table-body.tsx
+++ src/features/table/table-body.tsx
@@ -5,7 +5,6 @@ import {
   useComputed$,
   useOnWindow,
   useSignal,
-  useTask$,
   useVisibleTask$,
 } from '@builder.io/qwik';
 import { useExecution } from '~/features/add-column';
@@ -43,42 +42,37 @@ export const TableBody = component$(() => {
     );
   });
 
-  const data = useSignal<Cell[][]>([]);
-  useTask$(({ track }) => {
-    track(columns);
+  const getCell = (column: Column, rowIndex: number): Cell => {
+    const cell = column.cells[rowIndex];
 
-    const getCell = (column: Column, rowIndex: number): Cell => {
-      const cell = column.cells[rowIndex];
+    if (!cell) {
+      // Temporal cell for skeleton
+      return {
+        id: `${column.id}-${rowIndex}`,
+        value: '',
+        error: '',
+        validated: false,
+        column: {
+          id: column.id,
+        },
+        updatedAt: new Date(),
+        generated: false,
+        idx: rowIndex,
+      };
+    }
 
-      if (!cell) {
-        // Temporal cell for skeleton
-        return {
-          id: `${column.id}-${rowIndex}`,
-          value: '',
-          error: '',
-          validated: false,
-          column: {
-            id: column.id,
-          },
-          updatedAt: new Date(),
-          generated: false,
-          idx: rowIndex,
-        };
-      }
+    return cell;
+  };
 
-      return cell;
-    };
-
-    data.value = Array.from({ length: rowCount.value }, (_, rowIndex) =>
-      Array.from({ length: columns.value.length }, (_, colIndex) =>
-        getCell(columns.value[colIndex], rowIndex),
-      ),
-    );
-  });
+  const data = useSignal<number[][]>([]);
+  data.value = Array.from({ length: rowCount.value }, (_) =>
+    Array.from({ length: columns.value.length }, (_, colIndex) => colIndex),
+  );
 
   useOnWindow(
     'scroll',
     $((event) => {
+      console.log('scroll', (event.target as HTMLElement).scrollTop);
       scrollTop.value =
         (event.target as HTMLElement).scrollTop - tableBody.value!.offsetTop;
     }),
@@ -86,37 +80,46 @@ export const TableBody = component$(() => {
 
   return (
     <tbody ref={tableBody}>
-      {data.value
-        .slice(startIndex.value, endIndex.value)
-        .map((cells, rowIndex) => (
-          <tr key={rowIndex} class="hover:bg-gray-50/50 transition-colors">
-            {cells.map((cell) => {
-              return (
-                <Fragment key={`${rowIndex}-${cell.id}`}>
-                  {cell.column?.id === TEMPORAL_ID ? (
-                    <td class="min-w-80 w-80 max-w-80 px-2 min-h-[100px] h-[100px] border-[0.5px]" />
-                  ) : (
-                    <TableCell
-                      cell={cell}
-                      isExpanded={expandedRows.value.has(rowIndex)}
-                      onToggleExpand$={() => {
-                        const newSet = new Set(expandedRows.value);
-                        newSet.has(rowIndex)
-                          ? newSet.delete(rowIndex)
-                          : newSet.add(rowIndex);
-                        expandedRows.value = newSet;
-                      }}
-                    />
-                  )}
-                  {columnId.value === cell.column?.id && (
-                    <td class="min-w-[600px] w-[600px] bg-white" />
-                  )}
-                </Fragment>
-              );
-            })}
-            <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-r-0" />
-          </tr>
-        ))}
+      {data.value.slice().map((row, rowIndex) => (
+        <tr key={rowIndex} class="hover:bg-gray-50/50 transition-colors">
+          {row.map((col) => {
+            const column = columns.value[col];
+            const cell = getCell(column, rowIndex);
+
+            return (
+              <Fragment key={`${column.id}-${rowIndex}-${cell.id}`}>
+                {column.id === TEMPORAL_ID ? (
+                  <td
+                    key={`temporal-${rowIndex}`}
+                    class="min-w-80 w-80 max-w-80 px-2 min-h-[100px] h-[100px] border-[0.5px]"
+                  />
+                ) : (
+                  <TableCell
+                    cell={cell}
+                    isExpanded={expandedRows.value.has(rowIndex)}
+                    onToggleExpand$={() => {
+                      const newSet = new Set(expandedRows.value);
+                      if (newSet.has(rowIndex)) {
+                        newSet.delete(rowIndex);
+                      } else {
+                        newSet.add(rowIndex);
+                      }
+                      expandedRows.value = newSet;
+                    }}
+                  />
+                )}
+
+                {columnId.value === column.id && (
+                  <td class="min-w-[600px] w-[600px] bg-white" />
+                )}
+              </Fragment>
+            );
+          })}
+
+          {/* td for (add + ) column */}
+          <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-r-0" />
+        </tr>
+      ))}
     </tbody>
   );
 });

fad9f17729cfef05798ab1098511f5a5520d294a
Damián Pumar <damianpumar@gmail.com>
2025-02-28 21:01:25 +0100

    fixed header

--- src/features/table/table.tsx
+++ src/features/table/table.tsx
@@ -6,11 +6,18 @@ import { TableHeader } from '~/features/table/table-header';
 export const Table = component$(() => {
   return (
     <ExecutionProvider>
-      <div class="overflow-x-auto overflow-y-hidden h-full py-1">
-        <table class="min-w-max h-full border-separate border-spacing-0 text-sm">
-          <TableHeader />
-          <TableBody />
-        </table>
+      <div class="flex flex-col h-full">
+        <div class="sticky top-0 z-10 bg-white shadow-sm">
+          <table class="border-separate border-spacing-0 text-sm">
+            <TableHeader />
+          </table>
+        </div>
+
+        <div class="overflow-x-auto overflow-y-auto flex-grow">
+          <table class="border-separate border-spacing-0 text-sm">
+            <TableBody />
+          </table>
+        </div>
       </div>
     </ExecutionProvider>
   );

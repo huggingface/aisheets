ca2c0a0cd0e957835bbb6fe540e2d620ce1b2b4c
Damián Pumar <damianpumar@gmail.com>
2025-03-12 16:28:11 +0100

    feat: Update TableView layout and trigger button for improved usability

--- src/features/table/table-body.tsx
+++ src/features/table/table-body.tsx
@@ -159,7 +159,7 @@ export const TableBody = component$(() => {
 
             {/* td for (add + ) column */}
             {columns.value.filter((c) => c.id !== TEMPORAL_ID).length >= 1 && (
-              <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-t-0 border-r-0" />
+              <td class="min-w-80 w-80 max-w-80 min-h-[100px] h-[100px] border-[0.5px] border-r-0" />
             )}
           </tr>
         );

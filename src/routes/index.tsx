9f77ec0713721a6b3a1472381f98c6f2aca3cb4b
Damián Pumar <damianpumar@gmail.com>
2025-04-03 13:06:59 +0200

    [FEAT] Update sidebar branding and enhance dataset creation UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,8 @@
-import { $, component$, isDev, useSignal } from '@builder.io/qwik';
-import {
-  Link,
-  type RequestEvent,
-  server$,
-  useNavigate,
-} from '@builder.io/qwik-city';
+import { component$, isDev, useSignal } from '@builder.io/qwik';
+import { type RequestEvent, server$, useNavigate } from '@builder.io/qwik-city';
 import * as hub from '@huggingface/hub';
-import { LuDownload, LuFile, LuPencilLine, LuZap } from '@qwikest/icons/lucide';
-import { Tooltip } from '~/components/ui/tooltip/tooltip';
+import { LuEgg, LuFilePlus2 } from '@qwikest/icons/lucide';
+import { Button, Textarea } from '~/components';
 
 import { CLIENT_ID, HF_TOKEN, OAUTH_SCOPES } from '~/config';
 import { createDatasetIdByUser } from '~/services';
@@ -99,144 +94,41 @@ export default component$(() => {
   const isTransitioning = useSignal(false);
   const nav = useNavigate();
 
-  const handleCreateBlankDataset = $(async () => {
-    const datasetId = await createDataset();
-
-    nav(`/dataset/${datasetId}`);
-  });
-
-  const handleCreateBlankDatasetWithTransition = $(async () => {
-    isTransitioning.value = true;
-
-    const [datasetId] = await Promise.all([
-      createDataset(),
-      new Promise((resolve) => setTimeout(resolve, 400)),
-    ]);
-
-    nav(`/dataset/${datasetId}`);
-  });
-
   return (
     <ActiveDatasetProvider>
-      <div class="flex flex-col h-full w-fit overflow-hidden">
-        <div
-          class={`mt-12 w-[800px] transition-opacity duration-200 ${isTransitioning.value ? 'opacity-0' : 'opacity-100'}`}
-        >
-          <h1 class="text-3xl font-bold w-full mb-8">Choose how to start</h1>
+      <div class="w-full h-full flex flex-col items-center justify-center gap-6">
+        <h1 class="text-2xl font-medium">Design your data in a sheet</h1>
 
-          <div class="flex flex-col gap-0">
-            <Tooltip text="Coming soon!">
-              <div class="w-full text-[#676767] border-t border-b group transition-colors cursor-pointer">
-                <div class="w-full px-6 py-5 flex flex-row items-center gap-3 font-light group-hover:bg-gray-50/50">
-                  <span class="text-sm text-foreground">
-                    Generate content on specific topics.
-                  </span>
-                  <span class="text-[#AAB0C0] text-sm font-light">
-                    Create tweets, blog posts, or emails
-                  </span>
-                </div>
-              </div>
-            </Tooltip>
-
-            <Tooltip text="Coming soon!">
-              <div class="w-full text-[#676767] border-b group transition-colors cursor-pointer">
-                <div class="w-full px-6 py-5 flex flex-row items-center gap-3 font-light group-hover:bg-gray-50/50">
-                  <span class="text-sm text-foreground">
-                    Generate questions and responses.
-                  </span>
-                  <span class="text-[#AAB0C0] text-sm font-light">
-                    Produce reasoning, scientific, or creative writing questions
-                    and responses.
-                  </span>
-                </div>
-              </div>
-            </Tooltip>
-
-            <Tooltip text="Coming soon!">
-              <div class="w-full text-[#676767] border-b group transition-colors cursor-pointer">
-                <div class="w-full px-6 py-5 flex flex-row items-center gap-3 font-light group-hover:bg-gray-50/50">
-                  <span class="text-sm text-foreground">
-                    Generate code problems and solutions.
-                  </span>
-                  <span class="text-[#AAB0C0] text-sm font-light">
-                    Generate coding challenges and solutions.
-                  </span>
-                </div>
-              </div>
-            </Tooltip>
-
-            <div
-              class="w-full text-[#676767] border-b group transition-colors cursor-pointer"
-              onClick$={handleCreateBlankDataset}
-            >
-              <div class="w-full px-6 py-5 flex flex-row items-center gap-3 font-light group-hover:bg-gray-50/50">
-                <LuFile class="w-4 h-4 text-foreground" />
-                <span class="text-sm text-foreground">
-                  Create a blank dataset.
-                </span>
-                <span class="text-[#AAB0C0] text-sm font-light">
-                  Build synthetic datasets from scratch. Ideal to experiment
-                  with new models.
-                </span>
-              </div>
+        <div class="relative w-[600px] mt-6">
+          <div class="w-full h-96 min-h-96 max-h-96 bg-white border border-secondary-foreground rounded-sm">
+            <Textarea
+              id="prompt"
+              look="ghost"
+              class="p-4 h-80 min-h-80 max-h-80 resize-none overflow-auto text-base rounded-sm"
+            />
+          </div>
+          <div
+            class="w-full absolute bottom-2 right-2 flex flex-row items-center justify-between cursor-text"
+            onClick$={() => document.getElementById('prompt')?.focus()}
+          >
+            <div class="flex w-full justify-end items-center">
+              <Button look="primary" disabled>
+                <LuEgg class="text-2xl" />
+              </Button>
             </div>
-
-            <Link href="/dataset/create/from-hub" class="w-full">
-              <div class="w-full text-[#676767] border-b group transition-colors cursor-pointer">
-                <div class="w-full px-6 py-5 flex flex-row items-center gap-3 font-light group-hover:bg-gray-50/50">
-                  <LuDownload class="w-4 h-4 text-foreground" />
-                  <span class="text-sm text-foreground">
-                    Import a dataset from Hugging Face.
-                  </span>
-                  <span class="text-[#AAB0C0] text-sm font-light">
-                    Ideal for model evaluation, dataset transformation and
-                    augmentation.
-                  </span>
-                </div>
-              </div>
-            </Link>
           </div>
         </div>
+        <div class="w-[550px] flex justify-center items-center">
+          <hr class="w-full border-t border-gray-300" />
+          <span class="mx-4 text-gray-400">OR</span>
+          <hr class="w-full border-t border-gray-300" />
+        </div>
 
-        <div
-          class={`mt-16 text-primary-foreground font-light bg-white w-fit transition-all duration-1000 ${isTransitioning.value ? '-translate-y-[350px]' : ''}`}
-        >
-          <table class="border-separate border-spacing-0 text-sm">
-            <thead>
-              <tr class="min-h-8 h-8">
-                <th class="min-w-80 w-80 max-w-80 px-2 text-left border-[0.5px] border-r-0 border-b-0 rounded-tl-sm bg-neutral-100">
-                  <div class="flex items-center justify-between gap-2 w-full">
-                    <div class="flex items-center gap-2 text-wrap w-[80%] font-normal">
-                      <LuZap class="text-primary-foreground" />
-                      Column 1
-                    </div>
-                  </div>
-                </th>
-
-                <th class="min-w-80 w-80 max-w-80 px-2 text-left border-[0.5px] border-r-0 border-b-0 border-t-0 bg-neutral-100 relative">
-                  <div
-                    class={`absolute -top-6 left-[15%] -translate-x-1/2 bg-white shadow-md !rounded-none flex min-w-[240px] border border-[#eee] transition-opacity duration-200 h-[44px] cursor-pointer group ${isTransitioning.value ? 'opacity-0' : 'opacity-100'}`}
-                    onClick$={handleCreateBlankDatasetWithTransition}
-                  >
-                    <div class="flex items-center gap-2 px-4 w-full group-hover:bg-gray-50/50 transition-colors">
-                      <LuPencilLine class="w-4 h-4 text-[#676767]" />
-                      <span class="text-[#999] text-sm font-light">
-                        Start with a prompt
-                      </span>
-                    </div>
-                  </div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.from({ length: 8 }).map((_, i) => (
-                <tr key={i} class="hover:bg-gray-50/50 transition-colors">
-                  <td class="min-w-80 w-80 max-w-80 p-4 min-h-[100px] h-[100px] border-[0.5px] border-b-0 border-r-0 border-neutral-300" />
-                  <td class="min-w-80 w-80 max-w-80 p-4 min-h-[100px] h-[100px] border-[0.5px] border-b-0 border-r-0 border-neutral-300" />
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div class="w-[550px] flex justify-center items-center">
+          <Button class="flex gap-1">
+            <LuFilePlus2 class="text-lg" />
+            Drop or click to start with a file
+          </Button>
         </div>
       </div>
     </ActiveDatasetProvider>

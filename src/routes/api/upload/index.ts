ac89d0e918358e99bf9ae2db36af8016500c8a86
Francisco Aranda <frascuchon@gmail.com>
2025-04-03 12:47:00 +0200

    create dataset from uploaded file

--- src/routes/api/upload/index.ts
+++ src/routes/api/upload/index.ts
@@ -1,25 +1,63 @@
 import { createWriteStream, mkdirSync } from 'node:fs';
 import { join } from 'node:path';
 import type { RequestHandler } from '@builder.io/qwik-city';
-import { useServerSession } from '~/state';
+import { importDatasetFromFile } from '~/services/repository/datasets';
+import { type Session, useServerSession } from '~/state';
+
+import { DATA_DIR } from '~/config';
+
+import fs from 'node:fs/promises';
 
 export const onPost: RequestHandler = async (ev) => {
-  const { request } = ev;
-  const session = useServerSession(ev);
+  const { request, json } = ev;
 
-  const filename = request.headers.get('X-File-Name')!;
+  let filePath: string | undefined = undefined;
+
+  try {
+    const session = useServerSession(ev);
+    const filename = decodeURIComponent(request.headers.get('X-File-Name')!);
+
+    filePath = await writeRequestFileLocally(request, filename, session);
+
+    const newDataset = await importDatasetFromFile({
+      name: filename,
+      createdBy: session.user.username,
+      file: filePath,
+    });
+
+    json(201, newDataset);
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    json(500, { error: 'Failed to upload file' });
+  } finally {
+    // Clean up the file after processing
+    if (filePath) await deleteFile(filePath);
+  }
+};
+
+const writeRequestFileLocally = async (
+  request: Request,
+  filename: string,
+  session: Session,
+): Promise<string> => {
   const chunk = await request.arrayBuffer();
 
-  const chunksDir = join(process.cwd(), '/uploads', session.user.username);
+  const chunksDir = join(DATA_DIR, '/uploads/files');
   mkdirSync(chunksDir, { recursive: true });
 
-  const writeStream = createWriteStream(
-    join(chunksDir, decodeURIComponent(filename)),
-    {
-      flags: 'a',
-    },
-  );
+  const filePath = join(chunksDir, crypto.randomUUID()) + filename;
+  const writeStream = createWriteStream(filePath, { flags: 'a' });
 
   writeStream.write(Buffer.from(chunk));
   writeStream.end();
+
+  return filePath;
+};
+
+const deleteFile = async (filePath: string): Promise<void> => {
+  try {
+    await fs.unlink(filePath);
+  } catch (error) {
+    console.error('Error deleting file:', error);
+  }
 };

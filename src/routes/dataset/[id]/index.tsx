c969bc7f007c1b1dc5039a1f43e3052ef623a7dc
Francisco Aranda <frascuchon@gmail.com>
2025-04-08 14:37:03 +0200

    adapt current push to hug to save

--- src/routes/dataset/[id]/index.tsx
+++ src/routes/dataset/[id]/index.tsx
@@ -2,7 +2,7 @@ import { component$, useTask$ } from '@builder.io/qwik';
 import { type DocumentHead, useLocation } from '@builder.io/qwik-city';
 import { useActiveModal } from '~/components';
 import { DatasetName } from '~/features/datasets';
-import { ExportToHub } from '~/features/export-to-hub';
+import { ExportToHub } from '~/features/export/export-to-hub';
 import { Table } from '~/features/table';
 import { Username } from '~/features/user/username';
 

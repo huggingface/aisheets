435a2d675bd2cbff3819c0d0ab2690f9a1222e65
Damián Pumar <damianpumar@gmail.com>
2025-08-05 14:01:39 +0200

    feat: duplicate column

---
+++ src/features/table/components/header/duplicate-column.tsx
@@ -0,0 +1,42 @@
+import { $, component$ } from '@builder.io/qwik';
+import { LuLayers2 } from '@qwikest/icons/lucide';
+import { Button } from '~/components';
+import { useExecution } from '~/features/add-column';
+import { type Column, TEMPORAL_ID, useColumnsStore } from '~/state';
+
+export const DuplicateColumn = component$<{
+  column: Column;
+}>(({ column }) => {
+  const { open } = useExecution();
+  const { columns, addTemporalColumn } = useColumnsStore();
+
+  const onDuplicateColumn = $(async () => {
+    await addTemporalColumn(column.type);
+
+    open(
+      TEMPORAL_ID,
+      'add',
+      column.process?.prompt,
+      column.process?.modelName,
+      column.process?.modelProvider,
+    );
+  });
+
+  if (column.id === TEMPORAL_ID || columns.value.length <= 1) {
+    return null;
+  }
+
+  return (
+    <Button
+      class={
+        'p-2 cursor-pointer flex flex-row gap-1 items-center hover:bg-neutral-100 rounded-full w-full justify-start'
+      }
+      look="ghost"
+      size="sm"
+      onClick$={onDuplicateColumn}
+    >
+      <LuLayers2 class="text-sm text-neutral" />
+      Duplicate column
+    </Button>
+  );
+});

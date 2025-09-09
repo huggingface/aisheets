246afd0621f976d3e57995c85829c88ec6160123
Paco Aranda <francisco.aranda@huggingface.co>
2025-09-09 16:19:27 +0200

    [CHORE] Handling error when importing datasets from the hub (#425)
    return hub error as part of server response

--- src/usecases/import-from-hub.usecase.ts
+++ src/usecases/import-from-hub.usecase.ts
@@ -1,46 +1,64 @@
 import { type RequestEventBase, server$ } from '@builder.io/qwik-city';
-import { type Dataset, useServerSession } from '~/state';
-
+import type { HubApiError } from '@huggingface/hub';
 import consola from 'consola';
 import { importDatasetFromFile } from '~/services/repository/datasets';
 import { describeFromURI } from '~/services/repository/hub';
 import { downloadDatasetFile } from '~/services/repository/hub/download-file';
+import { useServerSession } from '~/state';
 
 export interface ImportFromHubParams {
   repoId: string;
   filePath: string;
+}
+export interface ImportFromHubResult {
+  dataset?: {
+    id: string;
+    name: string;
+  };
+
+  error?: string;
 }
 
 export const useImportFromHub = () =>
   server$(async function (
     this: RequestEventBase<QwikCityPlatform>,
     importParams: ImportFromHubParams,
-  ): Promise<Dataset> {
+  ): Promise<ImportFromHubResult> {
     const { repoId, filePath } = importParams;
     const session = useServerSession(this);
 
     consola.info('Downloading file', repoId, filePath);
-    const downloadedFilePath = await downloadDatasetFile({
-      repoId,
-      file: filePath,
-      accessToken: session.token,
-    });
+    try {
+      const downloadedFilePath = await downloadDatasetFile({
+        repoId,
+        file: filePath,
+        accessToken: session.token,
+      });
 
-    const fileInfo = await describeFromURI({
-      uri: downloadedFilePath,
-    });
+      const fileInfo = await describeFromURI({
+        uri: downloadedFilePath,
+      });
 
-    const totalRows = fileInfo.numberOfRows;
-    consola.info(`Importing ${totalRows} rows from ${filePath}`);
+      const totalRows = fileInfo.numberOfRows;
+      consola.info(`Importing ${totalRows} rows from ${filePath}`);
 
-    return await importDatasetFromFile(
-      {
-        name: `${repoId} [${filePath}]`,
-        createdBy: session.user.username,
-        file: downloadedFilePath,
-      },
-      {
-        limit: 1000,
-      },
-    );
+      const dataset = await importDatasetFromFile(
+        {
+          name: `${repoId} [${filePath}]`,
+          createdBy: session.user.username,
+          file: downloadedFilePath,
+        },
+        {
+          limit: 1000,
+        },
+      );
+
+      return { dataset };
+    } catch (error) {
+      consola.error('Error downloading file', error);
+
+      return {
+        error: `${(error as HubApiError).data?.error || (error as Error).message}`,
+      };
+    }
   });

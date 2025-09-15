c9903850eaa14c646973a6b4e1354832c2f39fdc
Damián Pumar <damianpumar@gmail.com>
2025-09-15 12:07:52 +0200

    feat: improve hover for opened execution form

--- src/features/table/components/header/cell-generation.tsx
+++ src/features/table/components/header/cell-generation.tsx
@@ -1,11 +1,14 @@
 import { component$ } from '@builder.io/qwik';
+import { cn } from '@qwik-ui/utils';
 import { LuEgg } from '@qwikest/icons/lucide';
 import { Tooltip } from '~/components/ui/tooltip/tooltip';
+import { useExecution } from '~/features/add-column';
 import { useGenerateColumn } from '~/features/execution';
 import { type Column, TEMPORAL_ID } from '~/state';
 
 export const CellGeneration = component$<{ column: Column }>(({ column }) => {
   const { onRegenerateCells } = useGenerateColumn();
+  const { columnId } = useExecution();
 
   if (column.id === TEMPORAL_ID || column.kind !== 'dynamic') return null;
   if (!column.process) return null;
@@ -13,7 +16,12 @@ export const CellGeneration = component$<{ column: Column }>(({ column }) => {
   return (
     <Tooltip text="Regenerate">
       <div
-        class="p-2 cursor-pointer transition-colors z-10 hover:bg-neutral-100 rounded-full"
+        class={cn(
+          'p-2 cursor-pointer transition-colors z-10 hover:bg-neutral-100 rounded-full',
+          {
+            'hover:bg-neutral-300': columnId.value === column.id,
+          },
+        )}
         onClick$={() => onRegenerateCells(column)}
         role="button"
         tabIndex={0}

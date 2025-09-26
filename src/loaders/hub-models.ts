c0f7262b4acfca7340857ee26480655bbf5e657a
Francisco Aranda <frascuchon@gmail.com>
2025-09-26 09:48:23 +0200

    cached values

--- src/loaders/hub-models.ts
+++ src/loaders/hub-models.ts
@@ -7,6 +7,7 @@ import {
 
 import { INFERENCE_PROVIDERS } from '@huggingface/inference';
 import { appConfig } from '~/config';
+import { cacheGet, cacheSet } from '~/services/cache';
 import { type Session, type TaskType, useServerSession } from '~/state';
 
 // This list helps to exclude providers that are not supported by the endpoint
@@ -144,6 +145,9 @@ const fetchModelsForPipeline = async (
   kind: TaskType,
   limit?: number,
 ): Promise<Model[]> => {
+  const cachedValue = cacheGet({ kind, limit });
+  if (cachedValue) return cachedValue as Model[];
+
   const url = 'https://huggingface.co/api/models';
 
   const params = new URLSearchParams([
@@ -225,6 +229,8 @@ const fetchModelsForPipeline = async (
       return acc;
     }, []) as Model[];
 
+    cacheSet({ kind, limit }, models);
+
     return models;
   } catch (error) {
     console.warn(`Unexpected error getting models`, String(error));
@@ -289,6 +295,7 @@ interface TrendingModel {
 export const useTrendingHubModels = routeLoader$(async function (
   this: RequestEventLoader,
 ): Promise<TrendingModel[]> {
+  console.log('Loading trending models...');
   const session = useServerSession(this);
   if (!session) return [];
 
